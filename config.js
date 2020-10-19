81a1652a02b320136550dd9991bdfa939d3cdc1b
MUSAB-NAIF12 <67227777+MUSAB-NAIF12@users.noreply.github.com>
2020-10-19 22:42:27 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  TOKEN: "NjE2NzA5NDk3ODE3MjY4Mjgx.XrhI9w.xkHllCuR15JwMoPm-BR95i6KI3Y", /// حط التوكن هنا
+  YT_API_KEY: "api ",
+  prefix: "1",
+  devs: ["616709497817268281"] ///  حط ايديك هنا
+};
